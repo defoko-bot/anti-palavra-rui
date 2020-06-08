3268d0fbe49542218fbf304f72328427ea7750d3
chocoboii~ <63813281+defoko-bot@users.noreply.github.com>
2020-06-08 18:22:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ bot.on("ready", () => {
 
 bot.on("message", (msg)=>  {
     const palavra = ["ayato", "exposed", "equisposed", "aiato"]
-    for(let i = 0; i = palavrar_proibida.length; i++){
+    for(let i = 0; i = palavra.length; i++){
         if(msg.content.includes(removemd(palavra[i]))){
             msg.delete()
             msg.channel.send(`${msg.author} n fala isso kraio`)
