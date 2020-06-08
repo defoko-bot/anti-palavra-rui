203028fd17d575c3a0f0757fffa0d6ceecbd6835
chocoboii~ <63813281+defoko-bot@users.noreply.github.com>
2020-06-08 18:15:26 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const antimarkdown = require('remove-markdown')
+const removemd = require('remove-markdown')
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
@@ -9,7 +9,7 @@ bot.on("ready", () => {
 bot.on("message", (msg)=>  {
     const palavrar_proibida = ["ayato", "exposed", "equisposed", "aiato"]
     for(let i = 0; i = palavrar_proibida.length; i++){
-        if(msg.content.includes(antimarkdown(palavrar_proibida[i]))){
+        if(msg.content.includes(removemd(palavrar_proibida[i]))){
             msg.delete()
             msg.channel.send(`${msg.author} n fala isso kraio`)
         }
